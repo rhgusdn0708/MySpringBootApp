318f9004763b5f79c6685ee98ec1042e53ebfa0d
rhgusdn0708 <hu070821@gmail.com>
2025-05-12 16:12:27 +0900

    auth

--- src/main/java/com/basic/myspringboot/auth/config/SecurityConfig.java
+++ src/main/java/com/basic/myspringboot/auth/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.basic.myspringboot.auth.config;
 
+import com.basic.myspringboot.auth.service.UserInfoUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,7 +32,8 @@ public class SecurityConfig {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
                     // /api/users/welcome 패스는 인증 없이 접근 가능한 경로
-                    auth.requestMatchers("/api/users/welcome").permitAll()
+                    // /userinfos/new 패스는 user_info 엔티티를 추가하는 경로는 인증 필요 없음
+                    auth.requestMatchers("/api/users/welcome","/userinfos/new").permitAll()
                             // /api/users/** 패스는 인증 후에만 접근 가능한 경로임
                             .requestMatchers("/api/users/**").authenticated();
                 })
@@ -38,18 +42,33 @@ public class SecurityConfig {
                 .build();
     }
 
+    // 개발자가 커스텀한 UserServiceDetail 서비스를 SpringBean으로 등록하기
     @Bean
-    //authentication
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User.withUsername("adminboot")
-                .password(encoder.encode("pwd1"))
-                .roles("ADMIN")  //관리자
-                .build();
-
-        UserDetails user = User.withUsername("userboot")
-                .password(encoder.encode("pwd2"))
-                .roles("USER") //사용자
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsService userDetailsService() {
+        return new UserInfoUserDetailsService();
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        // UserServiceDetail 서비스가 어떤 객체인지 알려주기
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        // 패스워드 인코더가 어떤 객체인지 알려주기
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//        UserDetails admin = User.withUsername("adminboot")
+//                .password(encoder.encode("pwd1"))
+//                .roles("ADMIN")  //관리자
+//                .build();
+//
+//        UserDetails user = User.withUsername("userboot")
+//                .password(encoder.encode("pwd2"))
+//                .roles("USER") //사용자
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 }
